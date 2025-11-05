68d338f512c30f669fcf29efffcbb8c4c0f5d164
woyteg <woyteg@gmail.com>
2025-11-04 23:38:05 -0600

    Change installation command in install.sh
    Updated installation command to reflect new repository URL.

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo ""
 # Check if running as root
 if [ "$EUID" -ne 0 ]; then 
     echo -e "${RED}ERROR: This script must be run as root${NC}"
-    echo "Please run: curl -sSL https://raw.githubusercontent.com/woyteg/minidashboard/main/install.sh | sudo bash"
+    echo "Please run: curl -sSL https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/install.sh | sudo bash"
     exit 1
 fi
 
